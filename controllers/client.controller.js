f323b116e384cc032d4abfc662be291ec02497a7
Matias N.Bergamaschi <matiasbergamaschi@gmail.com>
2022-11-12 18:24:30 -0300

    Clase3-CrearBorrarClientes

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,7 +1,7 @@
 import { clientServices } from "../services/client-service.js";
 
 // backticks ` `
-const crearNuevaLinea = (nombre, email) => {
+const crearNuevaLinea = (nombre, email, id) => {
     const linea =document.createElement("tr");
     const contenido = `
     <td class="td" data-td>
@@ -23,6 +23,7 @@ const crearNuevaLinea = (nombre, email) => {
         <button
             class="simple-button simple-button--delete"
             type="button"
+            id="${id}"
         >
             Eliminar
         </button>
@@ -31,14 +32,28 @@ const crearNuevaLinea = (nombre, email) => {
     </td>
     `;
     linea.innerHTML = contenido;
+    const btn = linea.querySelector("button");
+    btn.addEventListener("click", () => {
+        const id = btn.id;
+        clientServices
+            .eliminarCliente(id)
+            .then( (respuesta) => {
+                console.log(respuesta)
+        })
+        .catch(err => alert("Ocurrió un error!"))
+    });
+
     return linea;
 }
 
 const table = document.querySelector("[data-table]");
 
-clientServices.listaClientes().then((data) => {
-    data.forEach( (perfil) => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre,perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => alert("Ocurrió un error"));
+clientServices
+    .listaClientes()
+    .then((data) => {
+        data.forEach( ({ nombre, email, id }) => {
+        const nuevaLinea = crearNuevaLinea(nombre, email, id);
+            table.appendChild(nuevaLinea);
+        });
+})
+.catch((error) => alert("Ocurrió un error"));
